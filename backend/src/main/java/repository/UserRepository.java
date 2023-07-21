e7664f9c804c04fc7a7e96b9a94898d980d72c32
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-07-21 16:32:41 -0300

    added: main class and UserRepository

--- backend/src/main/java/repository/UserRepository.java
+++ backend/src/main/java/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package repository;
 
-public interface UserRepository extends {
+import model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    Page<User> findByPartOfTheName(@Param("name") String partOfTheName);
 }
---
+++ backend/src/main/java/service/UserService.java
@@ -0,0 +1,27 @@
+package service;
+
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User userToSave){
+        return userRepository.save(userToSave);
+
+    }
+
+    public Page<User> findByPartOfTheName(String partOfTheName){
+        return userRepository.findByPartOfTheName(partOfTheName);
+    }
+}
