64c052a43898dce9e26d303f319497ecda46e580
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-07-23 16:26:11 -0300

    managing the Repository

---
+++ backend/src/main/java/repository/MessageRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import model.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+
+
+}
