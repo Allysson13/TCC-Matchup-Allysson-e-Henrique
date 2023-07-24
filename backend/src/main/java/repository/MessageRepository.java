6569c71bb4208e91342cc45a42b1988ab2af7d6a
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-07-24 15:15:25 -0300

    adding methods for MessageRepository

--- backend/src/main/java/repository/MessageRepository.java
+++ backend/src/main/java/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package repository;
 
 import model.Message;
+import model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    /*@Query("SELECT m FROM Message m WHERE m.date LIKE %:date%")
+    Page<Message> findByPartOfTheDate(@Param("date") String partOfTheDate);*/
+    Page<Message> findByExtension(String extension);
+    Page<Message> findBySender(User sender);
 
+    Page<Message> findByReceiver(User receiver);
 
 }
