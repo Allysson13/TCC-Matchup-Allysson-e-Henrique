1c6ae9422488d144d7bea05930a8c053f1e94912
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-07-28 13:47:46 -0300

    added: attributes, constructors and encapsulation of the class "platform_interest"

--- backend/src/main/java/model/insterests_dependencies/Platform.java
+++ backend/src/main/java/model/insterests_dependencies/Platform.java
@@ -1,4 +1,39 @@
 package model.insterests_dependencies;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "platform_interest", schema = "matchup")
 public class Platform {
+
+    @Id
+    @Column(name="platform_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    // <editor-fold desc="Constructors">
+    public Platform() {
+    }
+
+    public Platform(String name) {
+        this.name = name;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Encapsulation">
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // </editor-fold>
 }
