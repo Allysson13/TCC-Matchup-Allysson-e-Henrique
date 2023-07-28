1b083e64ba1e9e800654833cf69cf52ccfa1f2c4
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-07-28 14:30:36 -0300

    added: new constructors and encapsulation of the Interest class

--- backend/src/main/java/model/Interest.java
+++ backend/src/main/java/model/Interest.java
@@ -54,11 +54,108 @@ public class Interest {
     public Interest() {
     }
 
+    public Interest(String name, String company, double price, List<Language> dubbing_interests, List<Language> subtitle_languages, List<Genre> genres, List<SubGenre> subGenre, List<Platform> platforms, AgeRating ageRating, List<User> users) {
+        this.name = name;
+        this.company = company;
+        this.price = price;
+        this.dubbing_interests = dubbing_interests;
+        this.subtitle_languages = subtitle_languages;
+        this.genres = genres;
+        this.subGenre = subGenre;
+        this.platforms = platforms;
+        this.ageRating = ageRating;
+        this.users = users;
+    }
+
+
 
     // </editor-fold>
 
     // <editor-fold desc="Encapsulation">
 
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Language> getDubbing_interests() {
+        return dubbing_interests;
+    }
+
+    public void setDubbing_interests(List<Language> dubbing_interests) {
+        this.dubbing_interests = dubbing_interests;
+    }
+
+    public List<Language> getSubtitle_languages() {
+        return subtitle_languages;
+    }
+
+    public void setSubtitle_languages(List<Language> subtitle_languages) {
+        this.subtitle_languages = subtitle_languages;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public List<SubGenre> getSubGenre() {
+        return subGenre;
+    }
+
+    public void setSubGenre(List<SubGenre> subGenre) {
+        this.subGenre = subGenre;
+    }
+
+    public List<Platform> getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(List<Platform> platforms) {
+        this.platforms = platforms;
+    }
+
+    public AgeRating getAgeRating() {
+        return ageRating;
+    }
+
+    public void setAgeRating(AgeRating ageRating) {
+        this.ageRating = ageRating;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
     // </editor-fold>
 
     public void addUser(User user){
