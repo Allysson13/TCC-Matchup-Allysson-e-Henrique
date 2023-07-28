9e28367ea601d5fcc8ceb0847edb589e84d411b9
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-07-28 14:42:16 -0300

    added: sql file, to add language list, int the directory "sql"

--- backend/src/main/java/repository/InterestRepository.java
+++ backend/src/main/java/repository/InterestRepository.java
@@ -15,20 +15,31 @@ public interface InterestRepository extends JpaRepository<Interest, Long> {
     @Query("SELECT i FROM Interest i WHERE i.name LIKE %:name%")
     Page<Interest> findByPartOfTheName(@Param("name") String partOfTheName);
 
-    @Query("SELECT i FROM Interest i WHERE i.genre LIKE %:genre%")
-    Page<Interest> findByPartOfTheGenre(@Param("genre") String partOfTheGenre);
-
-    @Query("SELECT i FROM Interest i WHERE i.subGenre LIKE %:sub_genre%")
-    Page<Interest> findByPartOfTheSubGenre(@Param("sub_genre") String partOfTheSubGenre);
-
-    @Query("SELECT i FROM Interest i WHERE i.company LIKE %:company%")
-    Page<Interest> findByPartOfTheCompany(@Param("company") String partOfTheCompany);
-
-    @Query("SELECT i FROM Interest i WHERE i.platform LIKE %:platform%")
-    Page<Interest> findByPartOfThePlatform(@Param("platform") String partOfThePlatform);
+//    @Query("SELECT i FROM Interest i WHERE i.genre LIKE %:genre%")
+//    Page<Interest> findByPartOfTheGenre(@Param("genre") String partOfTheGenre);
+//
+//    @Query("SELECT i FROM Interest i WHERE i.subGenre LIKE %:sub_genre%")
+//    Page<Interest> findByPartOfTheSubGenre(@Param("sub_genre") String partOfTheSubGenre);
+//
+//    @Query("SELECT i FROM Interest i WHERE i.company LIKE %:company%")
+//    Page<Interest> findByPartOfTheCompany(@Param("company") String partOfTheCompany);
+//
+//    @Query("SELECT i FROM Interest i WHERE i.platform LIKE %:platform%")
+//    Page<Interest> findByPartOfThePlatform(@Param("platform") String partOfThePlatform);
 
     //Price
 
     Page<Interest> findByAgeRating(int ageRating);
+
+
+
+
+
+
+    // <editor-fold desc="SQL to add Language List">
+    @Query()
+    void addLanguagesList();
+
+    // </editor-fold>
 
 }
