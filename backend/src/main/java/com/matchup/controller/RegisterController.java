a2f90b0953211151f9c9d10cc8ab72b59325579b
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-08-07 16:01:02 -0300

    configuring the endpoint for RegisterController

--- backend/src/main/java/com/matchup/controller/RegisterController.java
+++ backend/src/main/java/com/matchup/controller/RegisterController.java
@@ -1,9 +1,29 @@
 package com.matchup.controller;
 
+import com.matchup.model.Interest;
+import com.matchup.model.User;
+import com.matchup.service.InterestService;
+import com.matchup.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/register")
 public class RegisterController {
+
+    private final InterestService interestService;
+
+    @Autowired
+    public RegisterController(InterestService interestService) {
+        this.interestService = interestService;
+    }
+
+    @GetMapping("/api/admin/register-interest")
+    public ResponseEntity<Interest> register(@RequestBody Interest interest) {return new ResponseEntity<>(interestService.saveInterest(interest), HttpStatus.ACCEPTED);}
+
 }
