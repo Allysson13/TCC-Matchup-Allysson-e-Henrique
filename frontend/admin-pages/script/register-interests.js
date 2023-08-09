bd4e0abdc92f92bb845e0da6cad6238d59b7631e
Allysson Bruno Chaves Assunção <89218481+Allysson723@users.noreply.github.com>
2023-08-09 19:57:44 +0000

    current state, cors origin error

--- frontend/admin-pages/script/register-interests.js
+++ frontend/admin-pages/script/register-interests.js
@@ -1,42 +1,46 @@
-function loadDropDowns(){
-    
+function onLoad(){
+    loadDropDowns();
+    registerInterest();
 }
 
-function registerInterest(){
+function loadDropDowns() {
+    alert("DropDowns loaded!");
+}
+
+function registerInterest() {
     document.getElementById("register-interest").addEventListener("submit", function (event) {
         event.preventDefault(); 
-    
-        var formData = new FormData(document.getElementById("register-interest"));
-    
-        var jsonObject = {};
-        formData.forEach(function (value, key) {
-            jsonObject[key] = value;
-        });
 
-        console.log(jsonObject)
-        
-        
-        fetch("http://localhost:8080/api/admin/register-interest", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(jsonObject),
-            credentials: "include" // ou "same-origin", ou "omit"
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Erro ao enviar dados " + response);
-                }
-                return response.json();
-            })
-            .then(data => {
-                alert("Deu certo!");
-            })
-            .catch(error => {
-                alert("Deu errado!");
-            });
+    var formData = new FormData(document.getElementById("register-interest"));
+
+    var jsonObject = {};
+    formData.forEach(function (value, key) {
+        jsonObject[key] = value;
     });
-}
 
+    console.log(jsonObject);
+    alert("teste")
+
+    fetch("http://localhost:8080/api/admin/register-interest", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonObject),
+        credentials: "same-origin"
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao enviar dados " + response);
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert("Deu certo!");
+        })
+        .catch(error => {
+            alert("Deu errado! -> " + error);
+        });
+});
+}
 
