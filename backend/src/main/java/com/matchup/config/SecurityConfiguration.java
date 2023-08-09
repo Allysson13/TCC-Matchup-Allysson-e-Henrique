0c72bf961d257d197e1a5713e529295190a80eac
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-08-09 15:21:01 -0300

    configuring SecurityConfiguration

---
+++ backend/src/main/java/com/matchup/config/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.matchup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+//    @Bean
+//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+//        // Configure AuthenticationManagerBuilder
+//        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);// Get AuthenticationManager
+//        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+//        http
+//                .cors(Customizer.withDefaults())
+//                .csrf((csrf) -> csrf.disable())
+//                .authorizeHttpRequests((authz) -> authz
+//                        .requestMatchers(HttpMethod.POST).permitAll()
+//                        .anyRequest().authenticated())
+//
+//                .authenticationManager(authenticationManager)
+//                .sessionManagement((session) -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        return http.build();
+//    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "OPTIONS", "DELETE", "PATCH")); // or simply "*"
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
