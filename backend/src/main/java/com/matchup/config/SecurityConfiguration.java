f9de1e42ec8bea73cc31c345780f23b4f05a53e3
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-08-09 17:00:11 -0300

    testing to implement the WebMvcConfigurer to the SecurityConfiguration class

--- backend/src/main/java/com/matchup/config/SecurityConfiguration.java
+++ backend/src/main/java/com/matchup/config/SecurityConfiguration.java
@@ -14,13 +14,22 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration {
+public class SecurityConfiguration /*implements WebMvcConfigurer*/ {
+
+    /*@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }*/
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
