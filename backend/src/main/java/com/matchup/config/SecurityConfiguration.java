234a824affc7a3c3354d279729d5389776daca3c
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-08-10 16:05:53 -0300

    fixed: SecurityConfiguration

--- backend/src/main/java/com/matchup/config/SecurityConfiguration.java
+++ backend/src/main/java/com/matchup/config/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
-    /*@Bean
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // Configure AuthenticationManagerBuilder
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);// Get AuthenticationManager
@@ -62,6 +62,6 @@ public class SecurityConfiguration implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }*/
+    }
 
 }
