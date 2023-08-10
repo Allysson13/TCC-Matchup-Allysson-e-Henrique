f49c4bd87e77a0143f550be5e1db335d975a3167
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-08-10 14:21:56 -0300

    Frontend is finally making successful requests to the Backend

---
+++ backend/src/main/java/com/matchup/repository/interest_dependencies/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.matchup.repository.interest_dependencies;
+
+import com.matchup.model.insterests_dependencies.AgeRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Company extends JpaRepository<AgeRating, Long> {
+
+}
