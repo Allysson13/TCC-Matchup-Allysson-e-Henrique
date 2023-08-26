0f08a4c2af725aabf05a139c428a8df827e59703
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-08-26 17:22:30 -0300

    not much

--- frontend/user-pages/script/reset-password.js
+++ frontend/user-pages/script/reset-password.js
@@ -8,6 +8,61 @@ form.addEventListener("submit", function (event) {
     resetPassword();
 });
 
+async function checkAvailability(type, data) {
+    response = await fetch(`http://localhost:8080/api/data-verification/${type}/check-availability/${data}`)
+        .catch(error => {
+            alert("Deu errado! -> (checkAvailability)" + error);
+        });
+    return response;
+}
+
+txtPassword.addEventListener("input", function (event) {
+    validPassword = validatePassword(this.value);
+    changeInputBorder(validPassword, this);
+});
+
+
+txtConfirmedPassword.addEventListener("input", function (event) {
+    bothPasswordsEqual = (txtPassword.value == txtConfirmedPassword.value);
+    changeInputBorder(bothPasswordsEqual, this);
+});
+
+function changeInputBorder(validValue, element) {
+    if (!validValue) {
+        element.classList.add('is-invalid');
+    } else {
+        element.classList.remove('is-invalid');
+    }
+}
+
+function resetPassowrdRequisition(jsonObject, jsonObject2) {
+
+    fetch('http://localhost:8080/api/login/', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonObject + "/" + jsonObject2)
+    })
+        .then(async response => {
+            if (!response.ok) {
+                throw new Error("Senhas incompatíveis! " + response);
+            }else{
+                window.location.href = 'login.html';
+            }
+            addOptionToDropDown(type, await response.json());
+        })
+        .catch(error => {
+            alert("Deu errado! -> (reset-password())" + error);
+        });
+}
+
+async function resetPassowrd() {
+    if (!(await validateFields())) return;
+    console.log(txtEmailOrUsername.value);
+    resetPassowrdRequisition(txtPassword.value, txtConfirmedPassword.value);
+}
+
 /* function resetPassowrd() {
 
     let password = document.getElementById('txt-password').value;
