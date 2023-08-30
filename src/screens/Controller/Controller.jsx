82225b88a9fcfa7664203151cc0f4f41c89268bc
Allysson Bruno Chaves Assunção <89218481+Allysson723@users.noreply.github.com>
2023-08-30 18:29:42 +0000

    codesandbox test

---
+++ src/screens/Controller/Controller.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+export const Controller = () => {
+  return (
+    <div className="controller">
+      <div className="overlap-group">
+        <img
+          className="vector"
+          alt="Vector"
+          src="https://generation-sessions.s3.amazonaws.com/8b86f97026d14a446d871db95d6f7d6a/img/vector.svg"
+        />
+        <img
+          className="img"
+          alt="Vector"
+          src="https://generation-sessions.s3.amazonaws.com/8b86f97026d14a446d871db95d6f7d6a/img/vector-1.svg"
+        />
+      </div>
+    </div>
+  );
+};
