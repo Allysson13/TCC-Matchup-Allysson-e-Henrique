1f5434821a47cbe9bd279f50c0d1396801e268a3
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-09-10 19:37:25 -0300

    animation

--- frontend/src/components/AppIndex/ForWho.tsx
+++ frontend/src/components/AppIndex/ForWho.tsx
@@ -1,84 +1,83 @@
 import * as React from 'react';
-import { Theme } from '@mui/material/styles';
-import { SxProps } from '@mui/system';
+import {Theme} from '@mui/material/styles';
+import {SxProps} from '@mui/system';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import {Typography} from "@mui/material";
+import {Grow, Typography} from "@mui/material";
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import GroupsIcon from '@mui/icons-material/Groups';
+import SchoolIcon from '@mui/icons-material/School';
 
 const item: SxProps<Theme> = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  px: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    px: 5,
 };
 
 const ForWho = () => {
-  return (
-    <Box
-      component="section"
-      sx={{ display: 'flex', overflow: 'hidden', bgcolor: 'secondary.main' }}
-    >
-      <Container sx={{ mt: 15, mb: 30, display: 'flex', position: 'relative' }}>
+    function isScrolled() {
+        return window.scrollY > 100; // Defina o limite desejado
+    }
+
+    return (
         <Box
-          component="img"
-          src="/static/themes/onepirate/productCurvyLines.png"
-          alt="curvy lines"
-          sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
-        />
-        <Grid container spacing={5}>
-          <Grid item xs={12} md={4}>
-            <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues1.svg"
-                alt="suitcase"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                The best luxury hotels
-              </Typography>
-              <Typography variant="h5">
-                Para aqueles que tem muito tempo livre
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues2.svg"
-                alt="graph"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                New experiences
-              </Typography>
-              <Typography variant="h5">
-                Para os jovens pouco esperientes
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues3.svg"
-                alt="clock"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                Exclusive rates
-              </Typography>
-              <Typography variant="h5">
-                Para pessoas com renda estável
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  );
+            component="section"
+            sx={{display: 'flex', overflow: 'hidden', bgcolor: 'secondary.dark'}}
+        >
+            <Container sx={{mt: 20, mb: 25, display: 'flex', position: 'relative'}}>
+                {/*<Box
+                    component="img"
+                    src="/static/themes/onepirate/productCurvyLines.png"
+                    alt="curvy lines"
+                    sx={{pointerEvents: 'none', position: 'absolute', top: -180}}
+                />*/}
+
+                <Grid container spacing={5}>
+                    <Grow in={isScrolled()}
+                          style={{transformOrigin: '0 0 0'}}
+                          {...(isScrolled() ? {timeout: 2000} : {})}
+                    >
+                        <Grid item xs={12} md={4}>
+                            <Box sx={item}>
+                                <QuestionAnswerIcon
+                                    sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
+                                <Typography variant="h5" align="center">
+                                    Para pessoas que adoram conversar sobre jogos
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    </Grow>
+                    <Grow in={isScrolled()}
+                          style={{transformOrigin: '0 0 0'}}
+                          {...(isScrolled() ? {timeout: 1000} : {})}
+                    >
+                        <Grid item xs={12} md={4}>
+                            <Box sx={item}>
+                                <GroupsIcon
+                                    sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
+                                <Typography variant="h5" align="center">
+                                    Para gamers solitários em busca de mais companheiros de jogatina
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    </Grow>
+                    <Grow in={isScrolled()}>
+                        <Grid item xs={12} md={4}>
+                            <Box sx={item}>
+                                <SchoolIcon
+                                    sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
+                                <Typography variant="h5" align="center">
+                                    Para aqueles que desejam expandir o seu repertório gamer
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    </Grow>
+                </Grid>
+            </Container>
+        </Box>
+);
 }
 
 export default ForWho;
--- frontend/src/components/AppIndex/WhatIsMatchup.tsx
+++ frontend/src/components/AppIndex/WhatIsMatchup.tsx
@@ -1,94 +1,123 @@
 import * as React from 'react';
-import { Theme } from '@mui/material/styles';
-import { SxProps } from '@mui/system';
+import {Theme} from '@mui/material/styles';
+import {SxProps} from '@mui/system';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import {Button, Typography} from "@mui/material";
+import {Button, Grow, Typography, useScrollTrigger} from "@mui/material";
 import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
 import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
 
 const item: SxProps<Theme> = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  px: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    px: 5,
 };
 
 const number = {
-  fontSize: 24,
-  fontFamily: 'default',
-  color: 'secondary.main',
-  fontWeight: 'medium',
+    fontSize: 24,
+    fontFamily: 'default',
+    color: 'secondary.main',
+    fontWeight: 'medium',
 };
 
 const image = {
-  height: 55,
-  my: 4,
+    height: 55,
+    my: 4,
 };
 
 const WhatIsMatchup = () => {
-  return (
-    <Box
-      component="section"
-      sx={{ display: 'flex', bgcolor: 'black', overflow: 'hidden' }}
-    >
-      <Container
-        sx={{
-          mt: 10,
-          mb: 15,
-          position: 'relative',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+
+    function isScrolled() {
+        return window.scrollY > 3; // Defina o limite desejado
+    }
+
+    // Use o hook useScrollTrigger para detectar a rolagem
+    const scrolled = useScrollTrigger({
+        disableHysteresis: true, // O gatilho é ativado imediatamente quando você rola para cima
+        threshold: 0, // Defina o limite de deslocamento
+    });
+
+    return (
         <Box
-          component="img"
-          src="/static/themes/onepirate/productCurvyLines.png"
-          alt="curvy lines"
-          sx={{
-            pointerEvents: 'none',
-            position: 'absolute',
-            top: -180,
-            opacity: 0.7,
-          }}
-        />
-        <Typography variant="h4" component="h2" sx={{ mb: 14 }}> {/*marked="center"*/}
-          O que é o Matchup?
-        </Typography>
-        <div>
-          <Grid container spacing={5} sx={{marginBottom: '100px'}}>
-            <Grid item xs={12} md={4}>
-              <Box sx={item}>
-                <Box sx={number}>1.</Box>
-                  <SportsEsportsOutlinedIcon sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
-                <Typography variant="h5" align="center">
-                  Selecione seus jogos favoritos e converse sobre eles com seus amigos!
+            component="section"
+            sx={{display: 'flex', bgcolor: 'black', overflow: 'hidden'}}
+        >
+            <Container
+                sx={{
+                    mt: 10,
+                    mb: 15,
+                    position: 'relative',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Box
+                    component="img"
+                    src="/static/themes/onepirate/productCurvyLines.png"
+                    alt="curvy lines"
+                    sx={{
+                        pointerEvents: 'none',
+                        position: 'absolute',
+                        top: -180,
+                        opacity: 0.7,
+                    }}
+                />
+                <Typography variant="h4" component="h2" sx={{mb: 14}}> {/*marked="center"*/}
+                    O que é o Matchup?
                 </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Box sx={item}>
-                <Box sx={number}>2.</Box>
-                  <GroupAddOutlinedIcon sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
-                <Typography variant="h5" align="center">
-                  Encontre pessoas com gostos em comum para conversar pela plataforma!
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Box sx={item}>
-                <Box sx={number}>3.</Box>
-                  <SportsEsportsOutlinedIcon sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
-                <Typography variant="h5" align="center">
-                  Descubra novos jogos recomendados com base nos seus interesses!
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-        </div>
-        {/*<Button
+                <div>
+                    <Grid container spacing={5} sx={{marginBottom: '100px'}}>
+                        <Grow in={isScrolled()}>
+                            <Grid item xs={12} md={4}>
+                                <Box sx={item}>
+                                    <Box sx={number}>1.</Box>
+                                    <SportsEsportsOutlinedIcon
+                                        sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
+                                    <Typography variant="h5" align="center">
+                                        Selecione seus jogos favoritos e converse sobre eles com seus amigos!
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                        </Grow>
+                        <Grow in={isScrolled()}
+                              style={{transformOrigin: '0 0 0'}}
+                              {...(isScrolled() ? {timeout: 1000} : {})}
+                        >
+                            <Grid item xs={12} md={4}>
+                                <Box sx={item}>
+                                    <Box sx={number}>2.</Box>
+
+                                    <GroupAddOutlinedIcon
+                                        sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
+
+                                    <Typography variant="h5" align="center">
+                                        Encontre pessoas com gostos em comum para conversar pela plataforma!
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                        </Grow>
+                        <Grow in={isScrolled()}
+                              style={{transformOrigin: '0 0 0'}}
+                              {...(isScrolled() ? {timeout: 2000} : {})}
+                        >
+                            <Grid item xs={12} md={4}>
+                                <Box sx={item}>
+                                    <Box sx={number}>3.</Box>
+
+                                    <SportsEsportsOutlinedIcon
+                                        sx={{width: '80px', height: '80px', marginTop: '15px', marginBottom: '15px'}}/>
+                                    <Typography variant="h5" align="center">
+                                        Descubra novos jogos recomendados com base nos seus interesses!
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                        </Grow>
+                    </Grid>
+                </div>
+                {/*<Button
           color="secondary"
           size="large"
           variant="contained"
@@ -98,9 +127,10 @@ const WhatIsMatchup = () => {
         >
           Get started
         </Button>*/}
-      </Container>
-    </Box>
-  );
+            </Container>
+        </Box>
+    )
+        ;
 }
 
 export default WhatIsMatchup
