d0a2c9f1e47db60634b734ccc7855a1017b78086
henrique-leao-p <0063400@academico.ifmg.edu.br>
2023-09-09 21:09:24 -0300

    Changes by allysson

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,12 +1,16 @@
 import { createTheme } from '@mui/material/styles';
 import purple from '@mui/material/colors/purple';
-import {grey} from "@mui/material/colors";
+import {cyan, grey} from "@mui/material/colors";
+import {hexToRgb} from "@mui/material";
 
 const theme = createTheme({
         palette: {
             mode: "dark",
             primary: {
                 main: purple[700],
+            },
+            secondary: {
+                main: hexToRgb('#6D8C97'),
             },
             background: {
                 // default: grey[900],
